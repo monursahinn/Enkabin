b0330bec174dd392a7c1dcc81d992d2dd490fdf7
Onur <m.onursahin@hotmail.com>
2024-08-14 18:44:53 +0300

    Counter and top button update

--- js/main.js
+++ js/main.js
@@ -35,7 +35,7 @@
         }
     });
     $('.back-to-top').click(function () {
-        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
+        $('html, body').animate({ scrollTop: 0 }, 500, 'easeInOutExpo');
         return false;
     });
 
@@ -43,7 +43,7 @@
     // Facts counter
     $('[data-toggle="counter-up"]').counterUp({
         delay: 10,
-        time: 1000
+        time: 1500
     });
 
 
